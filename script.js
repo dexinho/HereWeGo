19c924d00095536427a8a7e10c66b70f2308d724
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-03 22:10:55 +0100

    feature/made enemies dynamic

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const canvas = document.querySelector("canvas");
-const c = canvas.getContext("2d");
+const CX = canvas.getContext("2d");
 canvas.width = 1000;
 canvas.height = 700;
 
@@ -10,8 +10,8 @@ class Player {
     }
 
     draw = () => {
-        c.fillStyle = "red";
-        c.fillRect(this.position.x, this.position.y, 50, 50);
+        CX.fillStyle = "red";
+        CX.fillRect(this.position.x, this.position.y, 50, 50);
     };
 
     update = () => {
@@ -19,31 +19,40 @@ class Player {
             if (
                 key === "ArrowRight" &&
                 keysPressed[key] &&
-                this.position.x + this.velocity.x < canvas.width - 50
+                this.position.x + this.velocity < canvas.width - 50
             )
-                this.position.x += this.velocity.x;
+                this.position.x += this.velocity;
             if (
                 key === "ArrowLeft" &&
                 keysPressed[key] &&
-                this.position.x - this.velocity.x > 0
+                this.position.x - this.velocity >= 0
             )
-                this.position.x += this.velocity.x * -1;
+                this.position.x += this.velocity * -1;
             if (
                 key === "ArrowUp" &&
                 keysPressed[key] &&
-                this.position.y + this.velocity.y > 0
+                this.position.y + this.velocity >= 0
             )
-                this.position.y += this.velocity.y * -1;
+                this.position.y += this.velocity * -1;
             if (
                 key === "ArrowDown" &&
                 keysPressed[key] &&
-                this.position.y + this.velocity.y < canvas.height - 50
+                this.position.y + this.velocity <= canvas.height - 50
             )
-                this.position.y += this.velocity.y;
+                this.position.y += this.velocity;
         }
 
         this.draw();
     };
+
+
+    get getVelocity() {
+        return this.velocity;
+    }
+
+    set setVelocity(newValue) {
+        this.velocity = newValue;
+    }
 }
 
 class Enemy {
@@ -59,8 +68,8 @@ class Enemy {
         this.position.y += this.velocity.y;
         this.position.x += this.velocity.x;
 
-        c.fillStyle = this.color;
-        c.fillRect(
+        CX.fillStyle = this.color;
+        CX.fillRect(
             this.position.x,
             this.position.y,
             this.size.width,
@@ -74,66 +83,65 @@ const player = new Player({
         x: canvas.width / 2 - 25,
         y: canvas.height - 100,
     },
-    velocity: {
-        x: 1,
-        y: 1,
-    },
+    velocity: 1,
 });
 
 const ENEMIES = [];
 let ENEMY_VELOCITY_X = 0;
 let ENEMY_VELOCITY_Y = 1;
-let ENEMY_SPAWN_SPEED = 500;
-let ENEMY_ID = 1;
+let ENEMY_SPAWN_SPEED = 1000;
+let OBSTACLE_ID = 1;
 
-function enemySpawn(){
-    const spawnInterval = setInterval(() => {
-        const enemy = new Enemy({
-            id: ENEMY_ID++,
-            position: {
-                x: Math.floor(Math.random() * canvas.width - 50),
-                y: -50,
-            },
-            size: {
-                width: Math.floor(Math.random() * 50) + 50,
-                height: Math.floor(Math.random() * 50) + 50,
-            },
-            velocity: {
-                x: ENEMY_VELOCITY_X * Math.floor(Math.random() * 3 - 1),
-                y: ENEMY_VELOCITY_Y,
-            },
-            color: `rgb(${Math.floor(Math.random() * 200) + 56}, 
-                ${Math.floor(Math.random() * 200 + 56)}, 
-                ${Math.floor(Math.random() * 200 + 56)})`,
-        });
-    
-        ENEMIES.push(enemy);
+let SPAWN_INTERVAL = setInterval(enemySpawn, ENEMY_SPAWN_SPEED);
 
-        console.log(ENEMY_SPAWN_SPEED)
-
-        if (ENEMIES.length % 10 === 0) {
-            ENEMY_SPAWN_SPEED -= 50
-            clearInterval(spawnInterval)
-            enemySpawn()
-        }
-    }, ENEMY_SPAWN_SPEED);
+function updateInverval() {
+    clearInterval(SPAWN_INTERVAL);
+    SPAWN_INTERVAL = setInterval(enemySpawn, ENEMY_SPAWN_SPEED);
 }
 
-enemySpawn()
+function enemySpawn() {
+    const obstacle = new Enemy({
+        id: OBSTACLE_ID++,
+        position: {
+            x: Math.floor(Math.random() * canvas.width - 50),
+            y: -50,
+        },
+        size: {
+            width: Math.floor(Math.random() * 50) + 50,
+            height: Math.floor(Math.random() * 50) + 50,
+        },
+        velocity: {
+            x: ENEMY_VELOCITY_X * Math.floor(Math.random() * 3 - 1),
+            y: ENEMY_VELOCITY_Y,
+        },
+        color: `rgb(${Math.floor(Math.random() * 200) + 56}, 
+            ${Math.floor(Math.random() * 200 + 56)}, 
+            ${Math.floor(Math.random() * 200 + 56)})`,
+    });
+
+    console.log(ENEMY_SPAWN_SPEED);
+
+    ENEMIES.push(obstacle);
+
+    if (ENEMIES.length % 10 === 0) {
+        ENEMY_SPAWN_SPEED -= 50;
+        updateInverval();
+    }
+}
 
 const enemyVelocityInterval = setInterval(() => {
-    
-
     if (ENEMY_VELOCITY_Y < 5) {
         ENEMY_VELOCITY_Y++;
+
+        player.setVelocity = ++player.getVelocity;
     }
     if (ENEMY_VELOCITY_X === 0) ENEMY_VELOCITY_X = 1;
-}, 5000);
+}, ENEMY_SPAWN_SPEED * 10);
 
 function animate() {
     window.requestAnimationFrame(animate);
-    c.fillStyle = "black";
-    c.fillRect(0, 0, canvas.width, canvas.height);
+    CX.fillStyle = "black";
+    CX.fillRect(0, 0, canvas.width, canvas.height);
     player.update();
     ENEMIES.forEach((enemy) => enemy.spawn());
 }
